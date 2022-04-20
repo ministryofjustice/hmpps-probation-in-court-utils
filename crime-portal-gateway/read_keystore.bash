c3835639c7f3c733625313ae8d4f1c39ad17dbf7
John Evans <john.evans@digital.justice.gov.uk>
2022-04-20 14:20:14 +0100

    ✨ Add read_keystore script for checking crime-portal-gateway keystore contents

---
+++ crime-portal-gateway/read_keystore.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+namespace=court-probation-dev
+
+# Read any named params
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+kubectl config use-context $namespace
+kubectl get secret crime-portal-gateway-keystore-cert -n $namespace -o json | jq -r '.data."crime-portal-gateway-jks.key"' | base64 -D > /tmp/crime-portal-gateway-jks.key
+KEYSTORE_PASSWORD=$(kubectl get secret crime-portal-gateway-secrets -n $namespace -o json | jq -r '.data | map_values(@base64d).KEYSTORE_PASSWORD')
+keytool -list -v -keystore /tmp/crime-portal-gateway-jks.key -storepass "$KEYSTORE_PASSWORD"
+rm /tmp/crime-portal-gateway-jks.key
