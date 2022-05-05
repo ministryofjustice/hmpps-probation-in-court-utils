17bf8908bb19880924d9eb6b27a18e7fdbf00a02
John Evans <john.evans@digital.justice.gov.uk>
2022-05-05 16:23:30 +0100

    ✨ Add URN to all cases and minor fixes (#8)

--- copy-s3.bash
+++ copy-s3.bash
@@ -26,6 +26,6 @@ export AWS_ACCESS_KEY_ID=$(echo "$secret_json" | jq -r .data.access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(echo "$secret_json" | jq -r .data.secret_access_key)
 export BUCKET_NAME=$(echo "$secret_json" | jq -r .data.bucket_name)
 
-OUTPUT_PATH=$output_folder/$BUCKET_NAME$bucket_path
+OUTPUT_PATH=$output_folder/$BUCKET_NAME/$bucket_path
 echo "🗂️ Copying files from '$BUCKET_NAME/$bucket_path' to '$OUTPUT_PATH'..."
 aws s3 cp s3://$BUCKET_NAME/$bucket_path $OUTPUT_PATH $options
