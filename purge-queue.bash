42b684f368455097078ebc4545c2346f71d74b0e
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-12-18 14:48:19 +0000

    Fix local use of these scripts

--- purge-queue.bash
+++ purge-queue.bash
@@ -31,7 +31,7 @@ then
   echo "🏠 Running against localstack"
   TOPIC_ARN="arn:aws:sns:eu-west-2:000000000000:court-cases-topic.fifo"
   OPTIONS="--endpoint-url http://localhost:4566"
-  QUEUE_URL="http://localhost:4566/000000000000/test-queue"
+  QUEUE_URL="http://localhost:4566/000000000000/court-case-matcher-queue.fifo"
   AWS_ACCESS_KEY_ID=
   AWS_DEFAULT_REGION=eu-west-2
 else
